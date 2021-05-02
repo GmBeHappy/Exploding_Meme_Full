b6df30b5360f9b6b1e0c37799ac98c14a30ec2eb
GmBeHappy <35629716+GmBeHappy@users.noreply.github.com>
2021-05-02 23:32:35 +0700

    update seat

--- src/exploding_meme_full/LobbyViewManger.java
+++ src/exploding_meme_full/LobbyViewManger.java
@@ -16,6 +16,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import exploding_meme_full.InfoLabel;
+import java.util.jar.Attributes;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.SpaceRunnerButton;
@@ -23,24 +24,24 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-public class LobbyViewManger 
-{
+public class LobbyViewManger {
+
     private Boolean host;
 
     private ImageView[] picture_player = new ImageView[6];
-        
+
     private InfoLabel chooseShipLabel1;
-    
+
     private InfoLabel[] textplayer_name = new InfoLabel[6];
-    
-    private String[] player_name ={"","","","","",""} ;
-    
-    
-    
+
+    private String[] player_name = {"1", "2", "3", "4", "5", "6"};
+
     private int number_people = 0;
 
-    private SpaceRunnerButton CreateButtonWait = new SpaceRunnerButton("WAIT",49,190);;
-    private SpaceRunnerButton CreateButtonSTART = new SpaceRunnerButton("START",49,190);;
+    private SpaceRunnerButton CreateButtonWait = new SpaceRunnerButton("WAIT", 49, 190);
+    ;
+    private SpaceRunnerButton CreateButtonSTART = new SpaceRunnerButton("START", 49, 190);
+    ;
     
     private Boolean gamestart = false;
 
@@ -48,22 +49,21 @@ public class LobbyViewManger
     private Scene gameScene;
     private Stage gameStage;
 
-
     private AnimationTimer TimerLoop;
 
-
-    private static final int GAME_WIDTH =  1920 ;
-    private static final int GAME_HIGHT =1080;
+    private static final int GAME_WIDTH = 1920;
+    private static final int GAME_HIGHT = 1080;
     private final static String BACKGROUND_IMAGE = "exploding_meme_full/resource/pokertable.png";
 
     private GridPane gridPane;
 
     private Stage menuStage;
-    
+    private String playerName;
+
     Lobby lobby;
 
-    public LobbyViewManger(String name)
-    {
+    public LobbyViewManger(String name) {
+        this.playerName=name;
         try {
             lobby = new Lobby(name);
         } catch (MqttException ex) {
@@ -71,9 +71,9 @@ public class LobbyViewManger
         }
         initializeStage();
     }
-    
-    public LobbyViewManger(String name, String code)
-    {
+
+    public LobbyViewManger(String name, String code) {
+        this.playerName=name;
         try {
             lobby = new Lobby(name, code);
         } catch (MqttException ex) {
@@ -83,33 +83,28 @@ public class LobbyViewManger
 
     }
 
-    private void initializeStage()
-    {
+    private void initializeStage() {
         gamePane = new AnchorPane();
-        gameScene = new Scene(gamePane,GAME_WIDTH,GAME_HIGHT);
+        gameScene = new Scene(gamePane, GAME_WIDTH, GAME_HIGHT);
         gameStage = new Stage();
         gameStage.setScene(gameScene);
     }
 
-    public void create(Stage menuStage,Boolean host) throws FileNotFoundException
-    {
+    public void create(Stage menuStage, Boolean host) throws FileNotFoundException {
         this.host = host;
         this.menuStage = menuStage;
         this.menuStage.hide();
         createBackground();
 
-        if(host == true)
-        {
-            setButtonSTART(1700,900);
+        if (host == true) {
+            setButtonSTART(1700, 900);
             gamePane.getChildren().add(CreateButtonSTART);
-        }
-        else 
-        {
-            setButtonWait(1700,900);
+        } else {
+            setButtonWait(1700, 900);
             gamePane.getChildren().add(CreateButtonWait);
         }
 
-        chooseShipLabel1 = new InfoLabel(String.format("%d / 6",number_people));
+        chooseShipLabel1 = new InfoLabel(String.format("%d / 6", number_people));
         chooseShipLabel1.setLayoutX(1500);
         chooseShipLabel1.setLayoutY(25);
         gamePane.getChildren().add(chooseShipLabel1);
@@ -118,57 +113,51 @@ public class LobbyViewManger
         gameStage.show();
     }
 
-    private void createBackground()
-    {
+    private void createBackground() {
         gridPane = new GridPane();
 
         ImageView backgroImageView1 = new ImageView(BACKGROUND_IMAGE);
         gridPane.getChildren().add(backgroImageView1);
 
-        
         gridPane.setLayoutX(280);
         gridPane.setLayoutY(50);
 
         gamePane.getChildren().add(gridPane);
     }
 
-    private void setButtonWait(int x,int y)
-    {
-        CreateButtonWait.setLayoutX(x); 
+    private void setButtonWait(int x, int y) {
+        CreateButtonWait.setLayoutX(x);
         CreateButtonWait.setLayoutY(y);
 
-        CreateButtonWait.setOnAction((event) -> 
-        {
-           
+        CreateButtonWait.setOnAction((event)
+                -> {
+
         });
-        
+
     }
 
-    private void setButtonSTART(int x,int y)
-    {
-        CreateButtonSTART.setLayoutX(x); 
+    private void setButtonSTART(int x, int y) {
+        CreateButtonSTART.setLayoutX(x);
         CreateButtonSTART.setLayoutY(y);
 
-        CreateButtonSTART.setOnAction((event) -> 
-        {
-           gamestart = true;
-            try { 
+        CreateButtonSTART.setOnAction((event)
+                -> {
+            gamestart = true;
+            try {
                 this.startGame();
             } catch (MqttException | InterruptedException ex) {
                 Logger.getLogger(LobbyViewManger.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
-        
+
     }
 
-    private void createGameLoop()
-    {
-        TimerLoop = new AnimationTimer(){
+    private void createGameLoop() {
+        TimerLoop = new AnimationTimer() {
 
             @Override
-            public void handle(long now) 
-            {   
-                if(!gamestart){
+            public void handle(long now) {
+                if (!gamestart) {
                     try {
                         checkgamestart();
                     } catch (MqttException | InterruptedException ex) {
@@ -178,98 +167,91 @@ public class LobbyViewManger
                 number_people = getPlayerInLobby();
                 update_picture_textpeople();
                 checkmouseposition();
+                updatePlayerInSeat();
             }
-            
+
         };
         TimerLoop.start();
     }
 
-    private void checkgamestart() throws MqttException, InterruptedException
-    {
-        if(host == true)
-        {
-            if(gamestart == true)
-            {
-                setButtonSTART(-100,-100);
-                this.startGame(); 
-            } 
-        }
-        else 
-        {
-            if(this.checkIsStart())
-            {
-                setButtonWait(-100,-100);
+    private void checkgamestart() throws MqttException, InterruptedException {
+        if (host == true) {
+            if (gamestart == true) {
+                setButtonSTART(-100, -100);
+                this.startGame();
+            }
+        } else {
+            if (this.checkIsStart()) {
+                setButtonWait(-100, -100);
                 lobby.startGame();
-            } 
+            }
         }
     }
 
+    private void update_picture_textpeople() {
+        chooseShipLabel1.setText(String.format("%d / 6", number_people));
+        for (int i = 0; i < 6; i++) {
+            if (player_name[i].equals("")) {
 
-    private void update_picture_textpeople()
-    {
-        chooseShipLabel1.setText(String.format("%d / 6",number_people));
-        for(int i =0;i<6;i++)
-        {
-            if(player_name[i].equals(""))
-            {
-
-            }
-            else
-            {
+            } else {
                 textplayer_name[i] = new InfoLabel(player_name[i]);
                 textplayer_name[i].setText(player_name[i]);
                 gamePane.getChildren().add(textplayer_name[i]);
 
-
                 picture_player[i] = new ImageView("exploding_meme_full/resource/player.png");
                 gamePane.getChildren().add(picture_player[i]);
-                switch(i) 
-                {
+                switch (i) {
                     case 0:
-                        picture_player[0].setLayoutX(384);
-                        picture_player[0].setLayoutY(109);
-                        textplayer_name[0].setLayoutX(350);
-                        textplayer_name[0].setLayoutY(246);
+                        picture_player[0].setLayoutX(905);
+                        picture_player[0].setLayoutY(760);
+                        textplayer_name[0].setLayoutX(750);
+                        textplayer_name[0].setLayoutY(693);
+
                         break;
                     case 1:
-                      picture_player[1].setLayoutX(905);
-                      picture_player[1].setLayoutY(38);
-                      textplayer_name[1].setLayoutX(750);
-                      textplayer_name[1].setLayoutY(180);
-                      break;
+                        picture_player[1].setLayoutX(335);
+                        picture_player[1].setLayoutY(691);
+                        textplayer_name[1].setLayoutX(400);
+                        textplayer_name[1].setLayoutY(600);
+
+                        break;
                     case 2:
-                      picture_player[2].setLayoutX(1420);
-                      picture_player[2].setLayoutY(120);
-                      textplayer_name[2].setLayoutX(1150);
-                      textplayer_name[2].setLayoutY(246);
-                      break;
+                        picture_player[2].setLayoutX(384);
+                        picture_player[2].setLayoutY(109);
+                        textplayer_name[2].setLayoutX(350);
+                        textplayer_name[2].setLayoutY(246);
+
+                        break;
                     case 3:
-                      picture_player[3].setLayoutX(1495);
-                      picture_player[3].setLayoutY(683);
-                      textplayer_name[3].setLayoutX(1150);
-                      textplayer_name[3].setLayoutY(600);
-                      break;
+                        picture_player[3].setLayoutX(905);
+                        picture_player[3].setLayoutY(38);
+                        textplayer_name[3].setLayoutX(750);
+                        textplayer_name[3].setLayoutY(180);
+
+                        break;
                     case 4:
-                      picture_player[4].setLayoutX(905);
-                      picture_player[4].setLayoutY(760);
-                      textplayer_name[4].setLayoutX(750);
-                      textplayer_name[4].setLayoutY(693);
-                      break;
+                        picture_player[4].setLayoutX(1420);
+                        picture_player[4].setLayoutY(120);
+                        textplayer_name[4].setLayoutX(1150);
+                        textplayer_name[4].setLayoutY(246);
+
+                        break;
                     case 5:
-                      picture_player[5].setLayoutX(335);
-                      picture_player[5].setLayoutY(691);
-                      textplayer_name[5].setLayoutX(400);
-                      textplayer_name[5].setLayoutY(600);
-                      break;
+                        picture_player[5].setLayoutX(1495);
+                        picture_player[5].setLayoutY(683);
+                        textplayer_name[5].setLayoutX(1150);
+                        textplayer_name[5].setLayoutY(600);
+
+                        break;
                     default:
-                      break;
-                  }
+                        break;
+                }
             }
         }
-        
+
     }
-    
-    private void startGame() throws MqttException, InterruptedException{
+
+    private void startGame() throws MqttException, InterruptedException {
         JSONObject msg = new JSONObject();
         msg.put("typeUpdate", "isStart");
         msg.put("status", "true");
@@ -277,32 +259,48 @@ public class LobbyViewManger
         System.out.println("ggggg");
         lobby.startGame();
     }
-    
-    private boolean checkIsStart(){
+
+    private boolean checkIsStart() {
         host = lobby.isIsStart();
         return lobby.isIsStart();
     }
-    
-    private void stopConnection() throws MqttException{
+
+    private void stopConnection() throws MqttException {
         lobby.disconnect();
     }
-    
-    private int getPlayerInLobby(){
+
+    private int getPlayerInLobby() {
         return lobby.getPlayerInLobby();
     }
-    
-     private void checkmouseposition()
-    {
-        gameScene.setOnMouseMoved((event) -> 
-        {
-            String msg =
-            "(x: "       + event.getX()      + ", y: "       + event.getY()       + ") -- " +
-            "(sceneX: "  + event.getSceneX() + ", sceneY: "  + event.getSceneY()  + ") -- " +
-            "(screenX: " + event.getScreenX()+ ", screenY: " + event.getScreenY() + ")";
 
-           //System.out.println(msg);
+    private void checkmouseposition() {
+        gameScene.setOnMouseMoved((event)
+                -> {
+            String msg
+                    = "(x: " + event.getX() + ", y: " + event.getY() + ") -- "
+                    + "(sceneX: " + event.getSceneX() + ", sceneY: " + event.getSceneY() + ") -- "
+                    + "(screenX: " + event.getScreenX() + ", screenY: " + event.getScreenY() + ")";
+
+            //System.out.println(msg);
         });
     }
-     
-     
+    
+    private void updatePlayerInSeat(){
+        int myIndex=0;
+        for (int i = 0; i < lobby.playerNames.size(); i++) {
+            if(lobby.playerNames.get(i).toString().equals(playerName)){
+                myIndex = i;
+            }
+        }
+        player_name[0]=lobby.playerNames.get(myIndex).toString();
+            if(lobby.playerNames.size()!=1){
+                for (int i = 1; i <= lobby.playerNames.size()-myIndex ; i++) {
+                player_name[0+i]=lobby.playerNames.get(myIndex+i).toString();
+            }
+            for (int i = 1; i >= myIndex ; i--) {
+                player_name[myIndex-i]=lobby.playerNames.get(myIndex-i).toString();
+            }
+        }
+        
+    }
 }
