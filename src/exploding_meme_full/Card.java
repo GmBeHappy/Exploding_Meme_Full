7c844edd6847fb5c2d46825df4e072639f04fa5c
Samach21 <69465282+Samach21@users.noreply.github.com>
2021-05-04 00:30:30 +0700

    updateCardClass

--- src/exploding_meme_full/Card.java
+++ src/exploding_meme_full/Card.java
@@ -7,6 +7,7 @@ import javafx.scene.shape.Rectangle;
 
 public class Card extends Parent{
     private int cardId;
+    private int index;
     private final String cardName;
     private final String[] cardSet = {"ATTACK", "FAVOR", "NOPE", "SEETHEFUTURE", "SHUFFLE", "SKIP", "NORMAL1", "NORMAL2", "NORMAL3", "NORMAL4", "NORMAL5", "DEFUSE", "EXPLODINGKITTEN"};
     private final Image image;
@@ -24,6 +25,14 @@ public class Card extends Parent{
         this.cardId = cardId;
         this.cardName = this.cardSet[cardId];
         this.image = randomImage(this.cardSet[cardId]);     
+        setImage();
+    }
+    
+    public Card(int cardId, int index) {
+        this.cardId = cardId;
+        this.index = index;
+        this.cardName = this.cardSet[cardId];
+        this.image = randomImage(this.cardSet[cardId], index);
         setImage();
     }
     
@@ -68,6 +77,11 @@ public class Card extends Parent{
     
     private Image randomImage(String cardName) {
         String output = cardName + String.valueOf(1 + (int)(Math.random() * 4));
+        return new Image("file:src/exploding_meme_full/resource/".concat(output.toLowerCase()).concat(".png"),100, 140, true, true);
+    }
+    
+    private Image randomImage(String cardName, int index) {
+        String output = cardName + String.valueOf(index);
         return new Image("file:src/exploding_meme_full/resource/".concat(output.toLowerCase()).concat(".png"),100, 140, true, true);
     }
 }
